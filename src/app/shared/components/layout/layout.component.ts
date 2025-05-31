1d08ff5b0fd3bd7836509faf47dee9fb18275b92
Dhairya Agarwal <dhairya.agarwal@nagarro.com>
2025-05-31 16:35:05 +0530

    navigation

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from '../nav/nav.component';
 import { CommonModule } from '@angular/common';
-
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-layout',
   standalone: true,
@@ -10,4 +11,15 @@ import { CommonModule } from '@angular/common';
   templateUrl:'./layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  showNav = true;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Hide nav on login and register routes
+      this.showNav = !(event.url === '/login' || event.url === '/register');
+    });
+  }
+}
